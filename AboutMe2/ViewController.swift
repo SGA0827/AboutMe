855148e98b76ec4699f453ab8410ba11932cb337
Sean Aldridge <aldridgesg@gmail.com>
2019-04-25 22:19:41 -0400

    Initial Commit

---
+++ AboutMe2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe2
+//
+//  Created by Sean Aldridge on 4/25/19.
+//  Copyright © 2019 Sean Aldridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
